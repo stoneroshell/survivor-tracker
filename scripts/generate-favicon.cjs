70d29456925529d03cd3a114e3f3dc8001a64cc9
Stone Roshell <stone.roshell14@gmail.com>
2026-02-21 12:21:30 -0800

    change favicon

--- scripts/generate-favicon.cjs
+++ scripts/generate-favicon.cjs
@@ -6,14 +6,15 @@ const sharp = require("sharp");
 const toIco = require("to-ico");
 
 const root = path.join(__dirname, "..");
-const srcPath = path.join(root, "public", "images", "og-image.png");
+const srcPath = path.join(root, "public", "images", "immunity-idol.svg");
 const outPath = path.join(root, "public", "favicon-v1.ico");
 
 async function main() {
   const input = fs.readFileSync(srcPath);
+  const sharpSvg = (buf) => sharp(buf, { density: 256 });
   const [buf16, buf32] = await Promise.all([
-    sharp(input).resize(16, 16).png().toBuffer(),
-    sharp(input).resize(32, 32).png().toBuffer(),
+    sharpSvg(input).resize(16, 16).png().toBuffer(),
+    sharpSvg(input).resize(32, 32).png().toBuffer(),
   ]);
   const ico = await toIco([buf16, buf32]);
   fs.writeFileSync(outPath, ico);
